4d4a0327a822e9a468c0c92ae61aedcf27004eab
hyejoo <joo95102@naver.com>
2023-02-19 03:36:19 +0900

    change again again

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 import HomeComponent from "./src/views/Home.js";
 import AboutComponent from "./src/views/About.js";
 import PostComponent from "./src/views/Post.js";
-import renderHeader from "./src/layouts/_Header.js";
-import renderFooter from "./src/layouts/_Footer.js";
+import renderHeader from "./src/layouts/Header.js";
+import renderFooter from "./src/layouts/Footer.js";
 
 function renderNotFound() {
   return "<h1>404 - Not found</h1>";
